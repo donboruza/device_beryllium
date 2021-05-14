bc986311e7452570cc4d3b0b1881a2bfd940cfe4
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    beryllium: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf

--- device.mk
+++ device.mk
@@ -345,6 +345,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
