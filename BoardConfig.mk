ea4cc68cc72bc600378e1642579412288a646ec6
Don boruza <109259258+donboruza@users.noreply.github.com>
2023-02-04 11:24:55 +0700

    beryllium:  switch to silvercore kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,7 +65,7 @@ BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc androidboot.i
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-TARGET_KERNEL_CONFIG += beryllium_defconfig
+TARGET_KERNEL_CONFIG += vendor/xiaomi/silvercore_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 TARGET_KERNEL_CLANG_VERSION := clang-proton
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/$(TARGET_KERNEL_CLANG_VERSION)
